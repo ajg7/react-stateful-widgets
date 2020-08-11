ec340a13943166736d5070f4f2fc3f08a64f0f66
ajg7 <ajgebara@gmail.com>
2020-08-10 21:59:41 -0500

    finished square.js

--- src/components/Squares.js
+++ src/components/Squares.js
@@ -14,23 +14,24 @@ Only one square (or none) can be active at any given point.
 Find comments below to help you along.
 */
 
-import React from 'react';
+import React, { useState } from 'react';
 
 // Use this variable ONLY to initialize a slice of state!
-const listOfSquareIds = ['sqA', 'sqB', 'sqC', 'sqD'];
-
 
 export default function Squares() {
   // Use the state hook twice, as we need two slices of state: 'squares' and
   // 'activeSquare'. One holds the _array_ of square ids, and the other keeps track
   // of the currently active square (if any).
+  const [activeSquareId, setActiveSquareId] = useState("");
+  const [listOfIds, setListOfIds] = useState(['sqA', 'sqB', 'sqC', 'sqD']);
 
   const isActive = id => {
     // This is not a click handler but a helper, used inside the JSX, (See below)
     // and should return a class name of active if the id passed
     // matches the active square in state, empty string otherwise.
     // Right-click and "inspect element" on the square to see its effect.
-    return ''
+    
+    return id === activeSquareId ? ' active' : "";
   };
 
   const markActive = id => {
@@ -38,6 +39,8 @@ export default function Squares() {
     // Set the id argument to be the active id in state
     // (unless it already is, in which case we should reset
     // the currently active square id back to initial state).
+
+    setActiveSquareId(id);
   };
 
   return (
@@ -48,7 +51,7 @@ export default function Squares() {
           /* Nasty bug! We should map over a slice of state, instead of 'listOfSquareIds'.
           We might say: "it works, though!" But if the list of squares is not state,
           we could never add squares, change squares or remove squares in the future. Fix!" */
-          listOfSquareIds.map(id =>
+          listOfIds.map(id =>
             <div
               id={id}
               key={id}
